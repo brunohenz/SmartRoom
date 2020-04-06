fb01f02fe9be75dbc3d08af79b913de8ce451893
Bruno Henz <brunohzsalomao@gmail.com>
2020-04-05 22:20:09 -0300

    Trainning algoritim and git ignore file

--- Tool/trainning.py
+++ Tool/trainning.py
@@ -2,4 +2,32 @@ import cv2
 import os
 import numpy as np
 
-eigenface = cv2.face
+eigenface = cv2.face.createEigenFaceRecognizer()
+fisherface = cv2.face.createFisherFaceRecognizer()
+lbph = cv2.face.createLBPHFaceRecognizer()
+
+def getImagemComID():
+    caminhos = [os.path.join('fotos', f) for f in os.listdir('fotos')]    
+    faces = []
+    ids = []
+    for caminhoImage in caminhos:
+        imagemFace = cv2.cvtColor(cv2.imread(caminhoImage), cv2.COLOR_BGR2GRAY)
+        id = int(os.path.split(caminhoImage) [-1].split('.')[1])        
+        ids.append(id)
+        faces.append(imagemFace)
+    return np.array(ids), faces
+
+ids, faces = getImagemComID()
+
+print("Treinando...")
+
+eigenface.train(faces, ids)
+eigenface.save('classificateEigen.yml')
+
+fisherface.train(faces, ids)
+fisherface.save('classificateFisher.yml')
+
+lbph.train(faces, ids)
+lbph.save('classificateLBPH.yml')
+
+print("Treinamento Realizado!")
